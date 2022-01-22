dab0ce021fa2b8c60d0c27bf4e307ec4fa1e1f67
AgeenkoKonstantin <kostik2000ag@gmail.com>
2022-01-22 22:45:04 +0300

    2nd step
    added message posting and find by tag

--- src/main/java/com/example/twitter/GreetingController.java
+++ src/main/java/com/example/twitter/GreetingController.java
@@ -1,15 +1,21 @@
 package com.example.twitter;
 
 
+import com.example.twitter.domain.Message;
+import com.example.twitter.repos.MessagesRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessagesRepo messagesRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -20,7 +26,33 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("some", "Hello, User!");
+
+        Iterable<Message> messages =  messagesRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag,Map<String, Object> model){
+        Message message = new Message(text, tag);
+        messagesRepo.save(message);
+
+        Iterable<Message> messages =  messagesRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter,Map<String, Object> model){
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()){
+            messages =  messagesRepo.findByTag(filter);
+        }else{
+            messages = messagesRepo.findAll();
+        }
+
+        model.put("messages", messages);
         return "main";
     }
 
---
+++ src/main/java/com/example/twitter/repos/MessagesRepo.java
@@ -0,0 +1,13 @@
+package com.example.twitter.repos;
+
+
+
+import com.example.twitter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface MessagesRepo extends CrudRepository<Message, Integer> {
+        List<Message> findByTag(String tag);
+}

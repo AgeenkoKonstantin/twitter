ef0f3c5b7c92047deac61587109c4c5946474511
AgeenkoKonstantin <kostik2000ag@gmail.com>
2022-01-24 16:23:34 +0300

    :(
    img load not work

--- src/main/java/com/example/twitter/domain/Message.java
+++ src/main/java/com/example/twitter/domain/Message.java
@@ -11,10 +11,19 @@ public class Message {
     private Integer id;
     private String text;
     private String tag;
-
+    private String filename;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+
 
     public String getAuthorName(){
         return author != null ? author.getUsername() : "<none>";
